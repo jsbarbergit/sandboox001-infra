4c39b990e0d2b0a7ce47aea001a830037a55b48b
Platform Portal API <jsbarber@gmail.com>
2023-08-07 08:18:35 +0000

    Added VPC resource: test001/10.0.0.0/24

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 
-        resource "aws_vpc" "test001" {
-          cidr_block = "10.0.0.0/24"
-          tags = {
-            Name = "test001"
-          }
-        }
+resource "aws_vpc" "test001" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "test001"
+  }
+}
     
